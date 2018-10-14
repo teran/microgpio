751a4b198cb0c6bd7c098d63a731ad48fb23abcb
Igor Shishkin <me@teran.ru>
2018-10-14 15:30:24 +0300

    Add client and test
    Signed-off-by: Igor Shishkin <me@teran.ru>

---
+++ client/client.go
@@ -0,0 +1,59 @@
+package client
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// Client type
+type Client struct {
+	endpoint string
+}
+
+// New returns new Client instance
+func New(endpoint string) *Client {
+	return &Client{
+		endpoint: endpoint,
+	}
+}
+
+// Ping method pings endpoint if it's available
+func (c *Client) Ping() error {
+	return c.request("GET", "/ping")
+}
+
+// Low method sets low bit to the pin with ID provided
+func (c *Client) Low(id int) error {
+	return c.request("POST", fmt.Sprintf("/gpio/%d/low", id))
+}
+
+// High sets high bit to the pin with ID provided
+func (c *Client) High(id int) error {
+	return c.request("POST", fmt.Sprintf("/gpio/%d/high", id))
+}
+
+// Output sets output mode for the pin with ID provided
+func (c *Client) Output(id int) error {
+	return c.request("POST", fmt.Sprintf("/gpio/%d/output", id))
+}
+
+func (c *Client) request(method, uri string) error {
+	httpClient := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	r, err := http.NewRequest(method, c.endpoint+uri, nil)
+	if err != nil {
+		return err
+	}
+
+	response, err := httpClient.Do(r)
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code received: %d", response.StatusCode)
+	}
+	return nil
+}
---
+++ client/client_test.go
@@ -0,0 +1,49 @@
+package client
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/teran/microgpio/drivers/fake"
+	"github.com/teran/microgpio/server"
+)
+
+type ClientTestSuite struct {
+	suite.Suite
+}
+
+func (s *ClientTestSuite) TestAll() {
+	driver := &fake.FakeDriver{
+		LowFunc: func(id int) error {
+			return nil
+		},
+		HighFunc: func(id int) error {
+			return nil
+		},
+		OutputFunc: func(id int) error {
+			return nil
+		},
+	}
+	srv := httptest.NewServer(server.New(driver))
+	defer srv.Close()
+
+	client := New(srv.URL)
+
+	err := client.Ping()
+	s.Require().NoError(err)
+
+	err = client.Output(1)
+	s.Require().NoError(err)
+
+	err = client.Low(1)
+	s.Require().NoError(err)
+
+	err = client.High(1)
+	s.Require().NoError(err)
+}
+
+func TestClientTestSuite(t *testing.T) {
+	suite.Run(t, &ClientTestSuite{})
+}
