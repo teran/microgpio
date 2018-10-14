530944dd8561612d0dc3e6fec358e68fe5bfba25
Igor Shishkin <me@teran.ru>
2018-10-14 18:35:49 +0300

    Add comments to fake driver
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- drivers/fake/fake.go
+++ drivers/fake/fake.go
@@ -1,19 +1,29 @@
 package fake
 
+import (
+	"github.com/teran/microgpio/drivers"
+)
+
+var _ drivers.Driver = &FakeDriver{}
+
+// FakeDriver type
 type FakeDriver struct {
 	LowFunc    func(int) error
 	HighFunc   func(int) error
 	OutputFunc func(int) error
 }
 
+// Output emulates setting pin to output mode
 func (f *FakeDriver) Output(id int) error {
 	return f.OutputFunc(id)
 }
 
+// Low emulates setting low bit to the pin specified
 func (f *FakeDriver) Low(id int) error {
 	return f.LowFunc(id)
 }
 
+// High emulates setting high bit to the pin specified
 func (f *FakeDriver) High(id int) error {
 	return f.HighFunc(id)
 }
