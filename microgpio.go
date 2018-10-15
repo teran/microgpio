a44c5d1d725bc1986040a7a2000fe4b644c2898b
Igor Shishkin <me@teran.ru>
2018-10-15 19:42:49 +0300

    Add ability to specify port to listen on
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- microgpio.go
+++ microgpio.go
@@ -3,15 +3,24 @@ package main
 import (
 	"log"
 
+	"github.com/kelseyhightower/envconfig"
+
 	"github.com/teran/microgpio/server"
 )
 
 // Version var to be set by linker
 var Version = "dev build"
 
+type config struct {
+	ListenAddr string `envconfig:"LISTEN_ADDR" default:":8080"`
+}
+
 func main() {
+	var cfg config
+	envconfig.MustProcess("microgpio", &cfg)
+
 	log.Printf("Starting microgpio=%s", Version)
 
 	srv := server.New()
-	log.Fatalf("%s", srv.ListenAndServe(":8080"))
+	log.Fatalf("%s", srv.ListenAndServe(cfg.ListenAddr))
 }
